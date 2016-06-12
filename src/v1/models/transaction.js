8ace1d13849c3aeb4a95a0bbf5d2ec85e0f6af20
caioflores <caioflores123@gmail.com>
2016-06-12 10:32:33 -0300

    simple payment commented

--- src/v1/models/transaction.js
+++ src/v1/models/transaction.js
@@ -5,14 +5,36 @@ const { DeviceSchema } = require('./device');
 const { Schema } = mongoose;
 const Device = require('../models/device');
 
+var Simplify = require("simplify-commerce"),
+client = Simplify.getClient({
+    publicKey: 'sbpb_MDYzNjdmYjYtOGFmMC00MWQ4LTg4MTEtYzM2YjBmM2VjYTlk',
+    privateKey: 'GXUTmeqkkh6jxtouxcZrv3tVLYl4KDr5KCIXldB99AB5YFFQL0ODSXAOkNtXTToq'
+});
+
+// client.payment.create(function(data) => {
+//     amount : "1000",
+//     token : "f21da65e-f0ab-45cb-b8e6-40b493c3671f",
+//     description : "payment description",
+//     currency : "USD"
+// }, function(errData, data){
+//     if(errData){
+//         console.error("Error Message: " + errData.data.error.message);
+//         // handle the error
+//         return;
+//     }
+//     console.log("Payment Status: " + data.paymentStatus);
+// });
+
 const TransactionSchema = new Schema({
   device: { type: Schema.Types.ObjectId, ref: 'Device', required: true },
   owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  value: { type: Number, required: true }
+  value: { type: Number, required: true },
+  token: { type: String, required: true },
 }, { timestamps: {} });
 
 TransactionSchema.post('save', (doc) => {
   Device.findByIdAndUpdate(doc.device, {$inc: {amountReceived: doc.value}}, {new: true}, console.log);
+  Simplify.pay(doc);
 });
 
 module.exports = mongoose.model('Transaction', TransactionSchema);
